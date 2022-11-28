8922cc0630fb4b93b0432180e04e7089e6f80b4a
Ryan Lee <ryan.lee@mavenwave.com>
2022-11-28 14:37:55 -0500

    addition of org_id

--- sharedvpc.tf
+++ sharedvpc.tf
@@ -1,7 +1,7 @@
 module "project-factory_example_shared_vpc" {
   source               = "terraform-google-modules/project-factory/google//examples/shared_vpc"
   version              = "14.1.0"
-  org_id               = "487033049345"
+  organization_id      = "487033049345"
   billing_account      = "0105E1-61A6DE-D85D10"
   folder_id            = "995591527023"
   host_project_name    = "rwl-gke-hostsvpc"
