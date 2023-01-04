e774d9e44f2b8f73b3f171ad5b5bfd2dda04f428
Ryan Lee <ryan.lee@mavenwave.com>
2023-01-04 13:57:04 -0500

    add gitignore file

---
+++ main.tf
@@ -0,0 +1,15 @@
+data "google_client_config" "default" {
+  provider = google
+}
+
+data "google_service_account_access_token" "default" {
+  provider               = google
+  target_service_account = "tf-import-sa@${var.project}.iam.gserviceaccount.com"
+  scopes                 = ["userinfo-email", "cloud-platform"]
+  lifetime               = "300s"
+}
+
+provider "google" {
+  alias        = "impersonated"
+  access_token = data.google_service_account_access_token.default.access_token
+}
