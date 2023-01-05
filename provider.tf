906f75e810da32f4f9c7956ff5738917e0662f07
Ryan Lee <ryan.lee@mavenwave.com>
2023-01-05 16:28:42 -0500

    update provider

--- provider.tf
+++ provider.tf
@@ -6,11 +6,18 @@ terraform {
     }
   }
 }
+provider "google" {
+  alias = "impersonation"
+  scopes = [
+    "https://www.googleapis.com/auth/cloud-platform",
+    "https://www.googleapis.com/auth/userinfo.email",
+  ]
+}
 locals {
   terraform_service_account = "tf-import-sa@${var.project}.iam.gserviceaccount.com"
 }
 data "google_service_account_access_token" "default" {
-  provider               = google
+  provider               = google.impersonation
   target_service_account = local.terraform_service_account
   scopes                 = ["userinfo-email", "cloud-platform"]
   lifetime               = "1200s"
@@ -18,7 +25,6 @@ data "google_service_account_access_token" "default" {
 
 provider "google" {
   project         = var.project
-  alias           = "impersonated"
   access_token    = data.google_service_account_access_token.default.access_token
   request_timeout = "60s"
 }
